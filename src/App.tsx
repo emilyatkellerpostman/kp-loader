62c82c8eb902d74b11afe9ca5a31de038fe5f0aa
Emily Negrette <emily.negrette@kellerpostman.com>
2023-09-21 16:41:04 -0500

    last changes in loader for animationn drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './index.css';
 import './App.css';
-import { motion } from 'framer-motion'
- 
-function App(){
-  return(
+import { motion } from 'framer-motion';
+
+function App() {
+  return (
     <div style={{
       paddingTop: "5rem",
       width: "100%",
@@ -12,18 +12,17 @@ function App(){
       justifyContent: "center"
     }}>
       <motion.svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 400 400"
-      className="item"
-      animate={{ 
-        scale: [1, 2, 1, 2, 1],
-      }}
-      transition={{duration: 2.5, repeat: Infinity}}
-      style={{ transformOrigin: "center" }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 400 400"
+        className="item"
+        animate={{
+          scale: [1, 2, 1, 2, 1],
+        }}
+        transition={{ duration: 2.5, repeat: Infinity }}
+        style={{ transformOrigin: "center" }}
       />
     </div>
   );
 }
 
 export default App;
-
--- src/ExampleTwo.tsx
+++ src/ExampleTwo.tsx
@@ -1,29 +1,67 @@
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 
-function ExampleTwo(){
-    return(
-        <div style={{
-            paddingTop: "5rem",
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            <motion.svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 400 400"
-            className="item"
-            animate={{ 
-              scale: [1, 2, 1, 2, 1],
-              rotate:  [0, 20, 40, 20, 0],
-            }}
-            transition={{duration: 2.5, repeat: Infinity}}
-            style={{ transformOrigin: "center",  }}
-            />
-          </div>
-        );
-      }
-      
-      export default ExampleTwo;
+interface DrawVariants extends Variants {
+  hidden: { opacity: number };
+  visible: {
+    opacity: number;
+    pathLength: number;
+    fillOpacity: number;
+    transition: {
+      duration: number;
+      ease: string;
+      repeat: number;
+    };
+  };
+}
 
+const draw: DrawVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fillOpacity: 1,
+    transition: {
+      duration: 2.5,
+      ease: "easeInOut",
+      repeat: Infinity
+    },
+  },
+};
+
+function ExampleTwo() {
+  return (
+    <div style={{
+      paddingTop: "5rem",
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <motion.svg
+        width="600"
+        height="600"
+        viewBox="0 0 600 600"
+        initial="hidden"
+        animate="visible"
+        variants={draw}
+        className="item"
+      >
+        <motion.path
+          d="M100 100 L200 200 L300 100 L400 200 L500 100"
+          fill="transparent"
+          stroke="black"
+          strokeWidth="5"
+          variants={draw}
+          style={{
+            pathLength: 0,
+            strokeDasharray: '0 1',
+            strokeDashoffset: 1,
+          }}
+        />
+      </motion.svg>
+    </div>
+  );
+}
+
+export default ExampleTwo;
 
